ef7e3b499eeec6e15ed29e5abeca659e79f3f1d2
mainbsl4 <mainbsl4@gmail.com>
2022-08-18 12:11:39 +0600

    hi

--- assets/js/products.js
+++ assets/js/products.js
@@ -9,17 +9,17 @@
 //     });
 // });
 
-function btnToggler (mainBtn, visible, hidden1, hidden2, hidden3, hidden4, hidden5, hidden6){
-    mainBtn.addEventListener("click", () => {
-        visible.classList.toggle("visible");
-        hidden1.classList.remove("visible");
-        hidden2.classList.remove("visible");
-        hidden3.classList.remove("visible");
-        hidden4.classList.remove("visible");
-        hidden5.classList.remove("visible");
-        hidden6.classList.remove("visible");
-    });
-};
+// function btnToggler (mainBtn, visible, hidden1, hidden2, hidden3, hidden4, hidden5, hidden6){
+//     mainBtn.addEventListener("click", () => {
+//         visible.classList.toggle("visible");
+//         hidden1.classList.remove("visible");
+//         hidden2.classList.remove("visible");
+//         hidden3.classList.remove("visible");
+//         hidden4.classList.remove("visible");
+//         hidden5.classList.remove("visible");
+//         hidden6.classList.remove("visible");
+//     });
+// };
 
 // btnToggler(mainBtn[0], products[0], products[1], products[2], products[3], products[4], products[5]);
 // btnToggler(mainBtn[1], products[1], products[0], products[2], products[3], products[4], products[5]);
