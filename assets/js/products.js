f96d5d7cb1b3203dc8ba2dfc0623cf5689f38bdd
mainbsl4 <mainbsl4@gmail.com>
2022-08-18 12:02:15 +0600

    xyz

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,5 +1,5 @@
-const mainBtn = document.querySelectorAll(".productContainer .forSlice .mainBtn");
-const products = document.querySelectorAll(".productContainer .forSlice .productBody");
+// const mainBtn = document.querySelectorAll(".productContainer .forSlice .mainBtn");
+// const products = document.querySelectorAll(".productContainer .forSlice .productBody");
 
 // mainBtn.forEach(mainBtn => {
 //     mainBtn.addEventListener("click", () => {
@@ -9,21 +9,21 @@ const products = document.querySelectorAll(".productContainer .forSlice .product
 //     });
 // });
 
-function btnToggler (mainBtn, visible, hidden1, hidden2, hidden3, hidden4, hidden5, hidden6){
-    mainBtn.addEventListener("click", () => {
-        visible.classList.toggle("visible");
-        hidden1.classList.remove("visible");
-        hidden2.classList.remove("visible");
-        hidden3.classList.remove("visible");
-        hidden4.classList.remove("visible");
-        hidden5.classList.remove("visible");
-        hidden6.classList.remove("visible");
-    });
-};
+// function btnToggler (mainBtn, visible, hidden1, hidden2, hidden3, hidden4, hidden5, hidden6){
+//     mainBtn.addEventListener("click", () => {
+//         visible.classList.toggle("visible");
+//         hidden1.classList.remove("visible");
+//         hidden2.classList.remove("visible");
+//         hidden3.classList.remove("visible");
+//         hidden4.classList.remove("visible");
+//         hidden5.classList.remove("visible");
+//         hidden6.classList.remove("visible");
+//     });
+// };
 
-btnToggler(mainBtn[0], products[0], products[1], products[2], products[3], products[4], products[5]);
-btnToggler(mainBtn[1], products[1], products[0], products[2], products[3], products[4], products[5]);
-btnToggler(mainBtn[2], products[2], products[1], products[0], products[3], products[4], products[5]);
-btnToggler(mainBtn[3], products[3], products[1], products[2], products[0], products[4], products[5]);
-btnToggler(mainBtn[4], products[4], products[1], products[2], products[3], products[0], products[5]);
-btnToggler(mainBtn[5], products[5], products[1], products[2], products[3], products[4], products[0]);
+// btnToggler(mainBtn[0], products[0], products[1], products[2], products[3], products[4], products[5]);
+// btnToggler(mainBtn[1], products[1], products[0], products[2], products[3], products[4], products[5]);
+// btnToggler(mainBtn[2], products[2], products[1], products[0], products[3], products[4], products[5]);
+// btnToggler(mainBtn[3], products[3], products[1], products[2], products[0], products[4], products[5]);
+// btnToggler(mainBtn[4], products[4], products[1], products[2], products[3], products[0], products[5]);
+// btnToggler(mainBtn[5], products[5], products[1], products[2], products[3], products[4], products[0]);
