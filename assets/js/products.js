f86d9006e4e4155f2a2151c745afc6f1f25e8b80
mainbsl4 <mainbsl4@gmail.com>
2022-08-18 13:27:34 +0600

    add products

--- assets/js/products.js
+++ assets/js/products.js
@@ -27,3 +27,46 @@
 // btnToggler(mainBtn[3], products[3], products[1], products[2], products[0], products[4], products[5]);
 // btnToggler(mainBtn[4], products[4], products[1], products[2], products[3], products[0], products[5]);
 // btnToggler(mainBtn[5], products[5], products[1], products[2], products[3], products[4], products[0]);
+
+
+
+const productCategories = document.querySelectorAll("#productCategories");
+
+
+const coil = document.querySelectorAll(".coil");
+const spray = document.querySelectorAll(".spray");
+const sanitizer = document.querySelectorAll(".sanitizer");
+const handWash = document.querySelectorAll(".handWash");
+const toiletCleaner = document.querySelectorAll(".toiletCleaner");
+const soap = document.querySelectorAll(".soap");
+
+function products (btn, visible, hidden1, hidden2, hidden3, hidden4, hidden5){
+    btn.addEventListener("click", ()=>{
+        visible.forEach(visible => {
+            visible.style.display = "block"
+        });
+        hidden1.forEach(hidden1 => {
+            hidden1.style.display = "none";
+        });
+        hidden2.forEach(hidden2 => {
+            hidden2.style.display = "none";
+        });
+        hidden3.forEach(hidden3 => {
+            hidden3.style.display = "none";
+        });
+        hidden4.forEach(hidden4 => {
+            hidden4.style.display = "none";
+        });
+        hidden5.forEach(hidden5 => {
+            hidden5.style.display = "none";
+        });
+    });
+};
+
+
+products(productCategories[0], coil, spray, sanitizer, handWash, toiletCleaner, soap);
+products(productCategories[1], spray, coil, sanitizer, handWash, toiletCleaner, soap);
+products(productCategories[2], sanitizer, spray, coil, handWash, toiletCleaner, soap);
+products(productCategories[3], handWash, spray, sanitizer, coil, toiletCleaner, soap);
+products(productCategories[4], toiletCleaner, spray, sanitizer, handWash, coil, soap);
+products(productCategories[5], soap, spray, sanitizer, handWash, toiletCleaner, coil);
