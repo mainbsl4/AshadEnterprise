50aaf8b27c07c9612706874680d764ea565f2db3
mainbsl4 <mainbsl4@gmail.com>
2022-08-18 11:30:26 +0600

    first commit

---
+++ assets/js/products.js
@@ -0,0 +1,29 @@
+const mainBtn = document.querySelectorAll(".productContainer .forSlice .mainBtn");
+const products = document.querySelectorAll(".productContainer .forSlice .productBody");
+
+// mainBtn.forEach(mainBtn => {
+//     mainBtn.addEventListener("click", () => {
+//         products.forEach(products => {
+//             products.classList.toggle("visible");
+//         });
+//     });
+// });
+
+function btnToggler (mainBtn, visible, hidden1, hidden2, hidden3, hidden4, hidden5, hidden6){
+    mainBtn.addEventListener("click", () => {
+        visible.classList.toggle("visible");
+        hidden1.classList.remove("visible");
+        hidden2.classList.remove("visible");
+        hidden3.classList.remove("visible");
+        hidden4.classList.remove("visible");
+        hidden5.classList.remove("visible");
+        hidden6.classList.remove("visible");
+    });
+};
+
+btnToggler(mainBtn[0], products[0], products[1], products[2], products[3], products[4], products[5]);
+btnToggler(mainBtn[1], products[1], products[0], products[2], products[3], products[4], products[5]);
+btnToggler(mainBtn[2], products[2], products[1], products[0], products[3], products[4], products[5]);
+btnToggler(mainBtn[3], products[3], products[1], products[2], products[0], products[4], products[5]);
+btnToggler(mainBtn[4], products[4], products[1], products[2], products[3], products[0], products[5]);
+btnToggler(mainBtn[5], products[5], products[1], products[2], products[3], products[4], products[0]);
